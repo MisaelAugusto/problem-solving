79c2538652ab2de24f57234a4b243348b8e12703
MisaelAugusto <misael.costa@ccc.ufcg.edu.br>
2026-03-24 22:39:57 -0300

    updated interval solution

--- beecrowd/1037-interval/solution.js
+++ beecrowd/1037-interval/solution.js
@@ -1,3 +1,14 @@
 const lines = [];
 
-// your code here
+const number = +lines[0];
+
+if (number >= 0 && number <= 25)
+  console.log('Intervalo [0,25]');
+else if (number > 25 && number <= 50)
+  console.log('Intervalo (25,50]');
+else if (number > 50 && number <= 75)
+  console.log('Intervalo (50,75]');
+else if (number > 75 && number <= 100)
+  console.log('Intervalo (75,100]');
+else
+  console.log('Fora de intervalo');
